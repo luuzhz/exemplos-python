f1b8ca5d83f02650af06c3b7b81a513948e5ca69
luuzhz <luisahansensilva@gmail.com>
2026-04-28 11:34:06 -0300

    Update 30-Dividir-Conta.py

--- 30-Dividir-Conta.py
+++ 30-Dividir-Conta.py
@@ -2,7 +2,7 @@ def calculo(total_conta,pessoas):
     resultado = total_conta / pessoas
     return resultado
 
-print("Seja Bem vindo ao Restaurante Caio Burguer")
+print("Seja Bem vindo ao Restaurante Benicio Pizaria")
 total_conta = float(input("Digite o Valor da Conta: "))
 pessoas = int(input("Digite a quantidade de Pessoas que a conta, será dividido: "))
 print(f"O valor a ser pago por cada um é :{calculo(total_conta,pessoas)}")
